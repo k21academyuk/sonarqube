ff00edf91efabaf51548d001e9f03345a8e8993f
k21academyuk <69501420+k21academyuk@users.noreply.github.com>
2024-06-22 11:43:22 +0530

    Update install_sonar_scanner.sh

--- scripts/install_sonar_scanner.sh
+++ scripts/install_sonar_scanner.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 cd /tmp || exit
 
 echo "Downloading sonar-scanner....."
-if [ -d "/tmp/sonar-scanner-cli-6.0.0.4432-linux.zip" ]; then
-    sudo rm /tmp/sonar-scanner-cli-6.0.0.4432-linux.zip
+if [ -e "/tmp/sonar-scanner-cli-6.0.0.4432-linux.zip" ]; then
+    rm /tmp/sonar-scanner-cli-6.0.0.4432-linux.zip
 fi
 
 wget -q https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-6.0.0.4432-linux.zip
@@ -17,12 +17,12 @@ rm sonar-scanner-cli-6.0.0.4432-linux.zip
 
 echo "Installing to /var/opt..."
 if [ -d "/var/opt/sonar-scanner-6.0.0.4432-linux" ]; then
-    sudo rm -rf /var/opt/sonar-scanner-6.0.0.4432-linux
+    rm -rf /var/opt/sonar-scanner-6.0.0.4432-linux
 fi
-sudo mv sonar-scanner-6.0.0.4432-linux /var/opt
+mv sonar-scanner-6.0.0.4432-linux /var/opt
 
 echo "Creating alias for sonar-scanner..."
-echo "alias sonar-scanner=/var/opt/sonar-scanner-6.0.0.4432-linux/bin/sonar-scanner" >> ~/.bashrc
-source ~/.bashrc
+echo "alias sonar-scanner='/var/opt/sonar-scanner-6.0.0.4432-linux/bin/sonar-scanner'" >> ~/.bashrc
+. ~/.bashrc
 
 echo "Installation and setup completed."
